e171260a8581ca1a99e6b60a8708f88279c87280
Isis Maximo Mello <isismaximomello@gmail.com>
2026-07-08 13:45:46 -0300

    geters e seters classe aluno

--- src/trabalhoFinal/Aluno.java
+++ src/trabalhoFinal/Aluno.java
@@ -5,4 +5,53 @@ public class Aluno extends Pessoa{
 	private int periodo;
 	private String turma;
 	private float notas; //array[10]
+	
+	public Aluno (){
+        super();
+        this.matricula = 0;
+        this.periodo = 0;
+    }
+
+    public Aluno(long matricula, int periodo) {
+        this.matricula = matricula;
+        this.periodo = periodo;
+    }
+
+    public Aluno(String nome, String data, long cpf, String contato,String matricula, String turma, int periodo) {
+        super(nome, data, cpf, contato);
+        this.matricula = matricula;
+        this.periodo = periodo;
+        this.turma = turma;
+    }
+    
+    public void setDados( String nome, String data, long cpf, String contato, String matricula, int periodo) {
+       super.SetDados(nome, data, cpf, contato);
+       this.matricula = matricula;
+       this.periodo = periodo;
+    }
+    
+    public void setmatricula(String matricula){
+       this.matricula = matricula;
+    }
+    
+    public void setperiodo(int periodo){
+       this.periodo=periodo;
+    }
+    
+    public String getmatricula(){
+        return this.matricula;
+    }
+    
+    public int getperiodo(){
+        return this.periodo;
+    }
+   
+    public void imprimeDados(){
+        System.out.println("nome: "+this.getNome());
+        System.out.println("cpf: "+this.getCpf());
+        System.out.println("Data Nascimento: "+this.getDataNascimento());
+        System.out.println("matricula: "+this.matricula);
+        System.out.println("periodo: "+this.periodo);
+    }
+	
 }
--- src/trabalhoFinal/Pessoa.java
+++ src/trabalhoFinal/Pessoa.java
@@ -10,4 +10,37 @@ public class Pessoa {
 	private String bairro;
 	private String cidade;
 	private String estado;
+	
+	public Pessoa(String nome, String data, long cpf, String contato) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void SetDados(String nome2, String data, long cpf2, String contato) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
 }
